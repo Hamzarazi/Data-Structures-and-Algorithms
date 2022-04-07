4d0970808ec0dae4dbb7cc37d8aa8ad3a5aec411
hamzarazi <hamzarazi2015@gmail.com>
2022-04-08 01:18:45 +0530

    Rotate Array Leetcode Solution minor changes

--- Rotate-Array.cpp
+++ Rotate-Array.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target)
+    void rev(vector<int>& nums , int i , int j)
+    {
+        while(i<j)
+        {
+            swap(nums[i] , nums[j]);
+            i++;
+            j--;
+        }
+    }
+    void rotate(vector<int>& nums, int k)
     {
         int n=nums.size();
-        int l=0 , r=n-1 , mid , ans;
-        while(l <= r)
-        {
-            mid=l+(r-l)/2;
-            if(target <= nums[mid])
-            {
-                r=mid-1;
-                ans=mid;
-            }
-            else
-                l=mid+1;
-        }
-        return ans;
+        if(k>n)
+            k=k%n;
+        rev(nums , 0 , n-1-k);
+        rev(nums , n-k , n-1);
+        rev(nums , 0 , n-1);
     }
 };
